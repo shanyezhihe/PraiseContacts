018a1eac9f2df3728778e2482f364c74b4220b2f
shanyezhihe <you@example.com>
2015-05-15 23:14:55 +0800

    主页面布局

--- PraiseContacts/gen/com/example/praisecontacts/R.java
+++ PraiseContacts/gen/com/example/praisecontacts/R.java
@@ -10,6 +10,10 @@ package com.example.praisecontacts;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int gray=0x7f040001;
+        public static final int green=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,25 +22,37 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int contacts_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int call=0x7f090002;
+        public static final int contacts=0x7f090003;
+        public static final int id_content=0x7f090000;
+        public static final int open_key_list=0x7f090001;
+        public static final int wearther=0x7f090004;
     }
     public static final class layout {
         public static final int activity_contact_main=0x7f030000;
+        public static final int activity_contacts_main=0x7f030001;
+        public static final int bottom_bar_contacts_page=0x7f030002;
+        public static final int fragment_contacts=0x7f030003;
+        public static final int fragment_weather=0x7f030004;
+        public static final int frament_call=0x7f030005;
+        public static final int titlebar_contacts_page=0x7f030006;
     }
     public static final class menu {
-        public static final int contact_main=0x7f070000;
+        public static final int contact_main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -60,10 +76,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- PraiseContacts/src/com/example/praisecontacts/ContactMainActivity.java
+++ PraiseContacts/src/com/example/praisecontacts/ContactMainActivity.java
@@ -1,20 +1,88 @@
 package com.example.praisecontacts;
 
-import android.app.Activity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 
-public class ContactMainActivity extends Activity {
-
+ public class ContactMainActivity extends FragmentActivity implements OnClickListener {
+	private Fragment callFragment;
+	private Fragment contactsFragment;
+	private Fragment weatherFragment;
+	private FragmentManager myFragmentManager;
+	private FragmentTransaction myFragmentTransaction;
+	private TextView TVcall;
+	private TextView TVcontacts;
+	private TextView TVweather;
+	
+	private int COLOR_GRAY = 0xFF7A7A7A;
+	private int COLOR_GREEN = 0xFF006400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_main);
+        setContentView(R.layout.activity_contacts_main);
         getActionBar().hide();
+        callFragment=new CallFrament();
+        contactsFragment=new ContactsFragment();
+        weatherFragment=new WeartherFrament();
+        TVcall=(TextView) findViewById(R.id.call);
+        TVcontacts=(TextView) findViewById(R.id.contacts);
+        TVweather=(TextView) findViewById(R.id.wearther);
+        TVcall.setOnClickListener(this);
+        TVcontacts.setOnClickListener(this);
+        TVweather.setOnClickListener(this);
+        initFrament();
     }
 
-
-  
+    public void initFrament()
+    {	TVcontacts.setTextSize(18);
+    	myFragmentManager=getSupportFragmentManager();
+    	myFragmentTransaction=myFragmentManager.beginTransaction();
+    	myFragmentTransaction.replace(R.id.id_content, contactsFragment).commit();
+    }
+ @Override
+	public void onClick(View v) {
+	 
+	    ContactMainActivity.this.myFragmentManager = getSupportFragmentManager();
+	    ContactMainActivity.this.myFragmentTransaction = myFragmentManager.beginTransaction();
+		
+		if(v.getId()==R.id.call)
+		{
+			myFragmentTransaction.replace(R.id.id_content,callFragment).commit();
+			TVcall.setTextColor(COLOR_GREEN);
+			TVcontacts.setTextColor(COLOR_GRAY);
+			TVweather.setTextColor(COLOR_GRAY);
+			TVcontacts.setTextSize(12);
+			TVweather.setTextSize(12);
+			TVcall.setTextSize(20);
+		}
+		if(v.getId()==R.id.contacts)
+		{
+			myFragmentTransaction.replace(R.id.id_content,contactsFragment).commit();
+			TVcall.setTextColor(COLOR_GRAY);
+			TVcontacts.setTextColor(COLOR_GREEN);
+			TVweather.setTextColor(COLOR_GRAY);
+			TVcontacts.setTextSize(20);
+			TVweather.setTextSize(12);
+			TVcall.setTextSize(12);
+		}
+		if(v.getId()==R.id.wearther)
+		{
+			myFragmentTransaction.replace(R.id.id_content,weatherFragment).commit();
+			TVcall.setTextColor(COLOR_GRAY);
+			TVcontacts.setTextColor(COLOR_GRAY);
+			TVweather.setTextColor(COLOR_GREEN);
+			TVweather.setTextSize(20);
+			TVcontacts.setTextSize(12);
+			TVcall.setTextSize(12);
+		}
+	}
+    
 }
